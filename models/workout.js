89a7709113033f294328a0d5b6ac97f7743ac9d7
Darin Wallace <dwalleye66@gmail.com>
2021-01-23 20:43:25 -0600

    server.js creation

--- models/workout.js
+++ models/workout.js
@@ -6,50 +6,20 @@ const workoutSchema = new Schema(
     {
         day: {
             type: Date,
-            default: Date.now
+            default: Date.now,
         },
         exercises: [
             {
-                type: {
-                    type: String,
-                    trim: true,
-                    required: "Enter an exercise type"
-                },
-                name: {
-                    type: String,
-                    trim: true,
-                    required: "Enter an exercise name"
-                },
-                duration: {
-                    type: Number,
-                    required: "Enter an exercise duration in minutes"
-                },
-                weight: {
-                    type: Number
-                },
-                reps: {
-                    type: Number
-                },
-                sets: {
-                    type: Number
-                },
-                distance: {
-                    type: Number
-                }
+                type: String,
+                name: String,
+                duration: Number,
+                weight: Number,
+                reps: Number,
+                sets: Number,
+                duration: Number,
+                distance: Number,
             }
         ]
-    },
-    {
-        toJSON: {
-            virtuals: true
-        }
-    }
-);
-
-workoutSchema.virtual("totalDuration").get(function () {
-    return this.exercises.reduce((total, exercise) => {
-        return total + exercise.duration;
-    }, 0);
 });
 
 const Workout = mongoose.model("Workout", workoutSchema);
