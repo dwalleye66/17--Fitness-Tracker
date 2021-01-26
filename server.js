cc6b374786fa4d037ce900135e0d30ebf4d06f41
Darin Wallace <dwalleye66@gmail.com>
2021-01-25 18:28:30 -0600

    modified server.js

--- server.js
+++ server.js
@@ -10,10 +10,15 @@ app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
-  useNewUrlParser: true,
-  useFindAndModify: false,
-});
+mongoose.connect(
+  process.env.MONGODB_URI || "mongodb://localhost/Fitness_Tracker",
+  {
+    useNewUrlParser: true,
+    useFindAndModify: false,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 app.use(require("./routes"));
 
